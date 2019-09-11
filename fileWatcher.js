f45b3b4c6227953fc6953b6846fa9963f1c17df1
AlexanderGonzo <alexgonzo1994@gmail.com>
2019-09-11 09:01:03 -0500

    Changed from watchFile to async function that will use song duration to calculate interval

--- fileWatcher.js
+++ fileWatcher.js
@@ -1,110 +1,63 @@
 /**
- * KTSW Notes: 
- * What does this script do? 
- * This Javascript file allows us to watch for any 
+ * KTSW Notes:
+ * What does this script do?
+ * This Javascript file allows us to watch for any
  * changes to our ktswplay.txt. If there are changes then the script
- * will add the artist, song, and record name to our database to be 
- * displayed to the user. 
- * 
+ * will add the artist, song, and record name to our database to be
+ * displayed to the user.
+ *
  * Troubleshooting:
  * If our service history functionality ever goes down, check here first
- * make sure the script is running. 
- * 
+ * make sure the script is running.
+ *
  * The command to run this script is node fileWatcher.js
- * 
- * Emergencies 
- * If you need further assistance contanct Alexander Gonzalez or Zach Collins. 
+ *
+ * Emergencies
+ * If you need further assistance contanct Alexander Gonzalez or Zach Collins.
  * If you do not have their contanct information ask Dan or your Station Manager
- * 
+ *
  * Script was written by Alexander Gonzalez 4/23/2019
- * 
+ *
  * Loop forever and sleep for 3 seconds and then send a request to get the latest data
  * request scope vs server scope
- * DO THIS BUT IN SERVICE SCOPE IT WILL LIVE FOREVER ADD DURATION TIME, THE GLOBAL VAR(SONG,ARTISTS,RECORD,DURATIONTIME) 
+ * DO THIS BUT IN SERVICE SCOPE IT WILL LIVE FOREVER ADD DURATION TIME, THE GLOBAL VAR(SONG,ARTISTS,RECORD,DURATIONTIME)
  * THE WATCHFILE WILL HAPPEN HERE. SET A TIMER USING THE DURATION TIME
- * 
- * TODO:
- * Create a counter=1000 erase the songs
- * Export the information to the DOM elements
- * instead of txt files we can also consider using Json files, and then
- * displaying the json in HTML via: https://www.w3schools.com/js/js_json_html.asp
- * 
  */
 const fs = require('fs');
-const nowPlayingFile = 'ktswplay.txt';
-
-var currentArtist = ' ';
-var currentSong = ' ';
-var currentRecord = ' ';
-
-let data = fs.readFileSync(nowPlayingFile, 'utf-8');
-var currentSongInfo = data.toString().split(/\s*(?:;|$)\s*/);
-
 var moment = require('moment');
-var currentTime = (moment().format('MMMM Do h:mm a')) + " ";
+var duration = 3000;
 
-var currentlyPlayingJSON = {};
+//TODO: Find a way to limit our DB to a week or two.
 
-var key = "Song"
-currentlyPlayingJSON[key] = [];
+async function textProcessing() {
 
+    try {
+        let data = fs.readFileSync('./ktswplay.txt', 'utf-8');
 
+        var currentSongInfo = data.toString().split(/\s*(?:;|$)\s*/);
+        var currentTime = (moment().format('MMMM Do h:mm a')) + " ";
+        var artist = currentSongInfo[0];
+        var song = currentSongInfo[1];
+        var record = currentSongInfo[2];
+        duration = 138000; //change to currentSongInfo[3] at a later date
 
-console.log(`Watching for file changes on ${nowPlayingFile}`);
+        var jsonData = fs.readFileSync('./songList.json');
+        var json = JSON.parse(jsonData);
 
-//Function used to watch our ktswplay.txt 
-// fs.watchFile(nowPlayingFile, { interval: 1000 }, (curr, prev) => {
+        var newSong = {
+            Time: currentTime,
+            Artist: artist,
+            Song: song,
+            Record: record,
+        };
+        json.push(newSong);
 
-//     currentArtist = currentSongInfo[0];
-//     currentSong = currentSongInfo[1];
-//     currentRecord = currentSongInfo[2];
-//     console.log("Artist: " + currentArtist);
-//     console.log("Song: " + currentSong);
-//     console.log("Record: " + currentRecord);
-    
-//     var stream = fs.createWriteStream("recentlyPlayed.txt", { flags: 'a' });
-//     stream.write(currentTime);
-//     currentSongInfo.forEach(function (item){
-//             stream.write(item + ' ' );
-           
-//     });
-//     stream.write('\n');
-//     stream.end(); 
-    
-// });
+        fs.writeFile('./songList.json', JSON.stringify(json), { flag: "w" }, (err) => {
+            if (err) throw err;
+            console.log("File has been saved");
+        });
 
-// fs.watchFile('./recentlyPlayed.txt', { interval: 1000 }, (curr, prev) => {
-//     var i;var count = 0;
-//     var tempData = currentTime + currentSongInfo + '\n';
-//     fs.createReadStream("./recentlyPlayed.txt")
-//         .on('error', e => callback(e))
-//         .on('data', chunk => {
-//             for (i=0; i < chunk.length; ++i) 
-//                 if (chunk[i] == 10) count++;
-//                 if(count > 1000){
-//                     fs.writeFile("./recentlyPlayed.txt", tempData , function(){console.log("count: " + count);})
-//                     count = 0;
-//                 }
-//         });
-// });
-fs.watchFile(nowPlayingFile, { interval: 5000 }, (curr, prev) => {   
-
-    var timeJson = {"Time": currentTime};
-    var artistJSON = { "Artist": currentSongInfo[0] };
-    var songJSON = { "Song": currentSongInfo[1] };
-    var recordJSON = { "Record": currentSongInfo[2] };
-    
-    currentlyPlayingJSON[key].push(timeJson);
-    currentlyPlayingJSON[key].push(artistJSON);
-    currentlyPlayingJSON[key].push(songJSON);
-    currentlyPlayingJSON[key].push(recordJSON);
-    
-    
-    let jsonData = JSON.stringify(currentlyPlayingJSON);
-    
-    console.log(currentlyPlayingJSON);
-    fs.writeFile('./currentlyPlaying.json',jsonData); 
-
-    fs.writeFile('./songList.json',songData,{flag: "a"});
-
-});
+    } catch (err) {
+        console.log(err);
+    }
+} setInterval(textProcessing, duration);
